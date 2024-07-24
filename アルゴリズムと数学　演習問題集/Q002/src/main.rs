d7735ab8b8076452ce7f9e115df6a09d49f589b1
TheRealPile <104041966+TheRealPile@users.noreply.github.com>
2024-07-25 01:14:51 +0900

    Delete アルゴリズムと数学　演習問題集 directory

--- アルゴリズムと数学　演習問題集/Q002/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::io;
-
-fn main() {
-  let mut input_line = String::new();
-  io::stdin().read_line(&mut input_line).expect("Failed to read line");
-  let numbers: Vec<i32> = input_line.trim().split_whitespace().map(|s| s.parse().expect("Please type a number!")).collect();
-  let sum: i32 = numbers.iter().sum();
-  println!("{}", sum);
-}
--- アルゴリズムと数学　演習問題集/Q006/src/main.rs
+++
@@ -1,9 +0,0 @@
-use std::io;
-
-fn main() {
-    let mut input = String::new();
-    io::stdin().read_line(&mut input).expect("Failed to read line.");
-    let n: i32 = input.trim().parse().expect("Please type a number!");
-    let result = n * 2 + 3;
-    println!("{}", result);
-}
