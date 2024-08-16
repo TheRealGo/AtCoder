0819f1e7c2ecff2f961f7f9eddf82159eb12d7ac
TheRealPile <v.greenultrabiso@gmail.com>
2024-08-16 12:16:46 +0900

    :zap: Improve performance.

--- math_and_algorithm/src/main.rs
+++ math_and_algorithm/src/main.rs
@@ -1,10 +1,12 @@
+use std::io;
+use std::io::Read;
+use std::io::BufRead;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Varius input types");
 }
 
 // 1行ずつ読み込む
-use std::io::{self, BufRead};
-
 fn line() {
     let stdin = io::stdin();
     let reader = stdin.lock();
@@ -16,8 +18,6 @@ fn line() {
 }
 
 // スペース区切りで複数の値を読み込む
-// use std::io;
-
 fn space() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
@@ -29,11 +29,8 @@ fn space() {
 }
 
 // 複数行にわたる複数のデータを読み込む
-// use std::io::{self, BufRead};
-
 fn lines() {
-    let stdin = io::stdin();
-    let mut iterator = stdin.lock().lines();
+    let mut iterator = io::stdin().lock().lines();
 
     while let Some(Ok(line)) = iterator.next() {
         let numbers: Vec<i32> = line.split_whitespace()
@@ -43,13 +40,9 @@ fn lines() {
     }
 }
 
-// 高度な入力パース
-// use std::io::{self, BufRead};
-
+// 行ごとに処理をする
 fn parse() {
-    let stdin = io::stdin();
-    let reader = stdin.lock();
-    let mut lines = reader.lines();
+    let mut lines = io::stdin().lock().lines();
 
     let first_line = lines.next().unwrap().unwrap();
     let n: i32 = first_line.parse().unwrap();
@@ -59,4 +52,23 @@ fn parse() {
             println!("{}", line);
         }
     }
+}
+
+// 一度に文書全体を読み込む
+fn parse_input() {
+    let mut input = String::new();
+    io::stdin().read_to_string(&mut input).unwrap();
+
+    let mut iter = input.split_whitespace();
+    let n: usize = iter.next().unwrap().parse().unwrap();
+
+    let mut x = Vec::with_capacity(n);
+    let mut y = Vec::with_capacity(n);
+
+    for _ in 0..n {
+        x.push(iter.next().unwrap().parse::<u32>().unwrap());
+        y.push(iter.next().unwrap().parse::<u32>().unwrap());
+    }
+
+    println!("{:?}, {:?}", x, y);
 }
