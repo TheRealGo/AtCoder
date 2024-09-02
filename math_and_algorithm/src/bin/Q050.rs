16081e35d8c1c04d418807e889d944d6226ec0ae
TheRealPile <v.greenultrabiso@gmail.com>
2024-09-02 23:05:54 +0900

    :zap: Improve performance.

--- math_and_algorithm/src/bin/Q050.rs
+++ math_and_algorithm/src/bin/Q050.rs
@@ -6,13 +6,11 @@ fn main() {
 
     let mut result: u64 = 1;
     while b > 0 {
-        if b % 2 == 1 {
-            result *= a;
-            result %= MOD;
+        if b & 1 == 1 {
+            result = (result * a) % MOD;
         }
-        a *= a;
-        a %= MOD;
-        b /= 2;
+        a = (a * a) % MOD;
+        b >>= 1;
     }
     println!("{}", result);
 }
