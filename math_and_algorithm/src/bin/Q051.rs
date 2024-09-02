97299716ac8ffc188fd7997a8222d0548df3536f
TheRealPile <v.greenultrabiso@gmail.com>
2024-09-03 01:29:46 +0900

    :zap: Improve performance.

--- math_and_algorithm/src/bin/Q051.rs
+++ math_and_algorithm/src/bin/Q051.rs
@@ -1,18 +1,18 @@
 use proconio::input;
-const MOD: usize = 1000000007;
+const MOD: u64 = 1000000007;
 
 fn main() {
-    input! {x: usize, y: usize}
+    input! {x: u64, y: u64}
 
-    let mut factorial: usize = 1;
-    let mut n: usize = 1;
+    let mut factorial: u64 = 1;
+    let mut n: u64 = 1;
     for i in 2..x + y + 1 {
         factorial = (factorial * i) % MOD;
         if i == x { n = (n * factorial) % MOD };
         if i == y { n = (n * factorial) % MOD };
     }
 
-    let mut inverse: usize = 1;
+    let mut inverse: u64 = 1;
     let mut p = MOD - 2;
     while p > 0 {
         if p & 1 == 1 {
